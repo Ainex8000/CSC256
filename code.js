f970ec0358c23e5b5b9744a0e89c3d519db2fe91
Xenia DeNoyer <90346767+Ainex8000@users.noreply.github.com>
2023-02-05 14:10:54 -0700

    Delete code.js

--- code.js
+++
@@ -1,7 +0,0 @@
-function displayText() {
-    document.getElementById("myText").innerHTML = "Welcome to the new page";
-}
-
-function displayText2() {
-    document.getElementById("myText2").innerHTML = "Fun fact: I have three puppies";
-}
